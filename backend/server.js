6df968c7a0479452049840172947a1547a0c49e1
Serkan Durmaz <serkan.durmaz.ce@gmail.com>
2025-03-05 15:18:58 +0300

    Backendi frontende bağladık.

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,21 @@ app.use(cors());
 //     .catch((err) => console.error("MongoDB bağlantı hatası:", err));
 
 // Örnek API Endpoint
+
+// Basit bir veri
+const users = [
+    { id: 1, username: 'user1' },
+    { id: 2, username: 'user2' },
+    { id: 3, username: 'user3' },
+];
+
+
+// Basit GET Route
+app.get('/api/users', (req, res) => {
+    res.json(users); // Kullanıcıları JSON formatında döndür
+});
+
+
 app.get("/", (req, res) => {
     res.send("Server Çalışıyor...");
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    // Backend'den kullanıcıları al
+    axios
+      .get('http://localhost:5000/api/users')  // Backend endpoint'ine GET isteği
+      .then((response) => {
+        setUsers(response.data); // Kullanıcıları state'e set et
+      })
+      .catch((error) => {
+        console.error('Veri çekme hatası:', error);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>Kullanıcılar</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
+  ;
 }
 
 export default App
