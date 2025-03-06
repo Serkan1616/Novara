8c506cd202b78e922b97658cb7d923e97a8d4006
Yasin Atıcı <yasinatici273@gmail.com>
2025-03-06 16:18:53 +0300

    recommandation eklendi.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,19 +3,19 @@ import { Route, Routes, Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import ProductCard from '../components/ProductCard';
 import NavCategoryMenu from '../components/NavCategoryMenu';
+import Recommandation from '../components/Recommandation';
+
 
 const HomePage = () => {
   return (
-    <>
+    <div className='mx-[200px]'>
       {/* <NavBar /> */}
       <NavCategoryMenu />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      <Recommandation />
 
       <Link to="/Login">Login</Link>
       <Link to="/register">Register</Link>
-    </>
+    </div>
   )
 }
 
