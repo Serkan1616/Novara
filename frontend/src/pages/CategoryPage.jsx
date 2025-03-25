ca74ca3e5b9573c3406aa13c5284630998eaeae4
Yasin Atıcı <yasinatici273@gmail.com>
2025-03-25 14:02:26 +0300

    degisiklik

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -40,22 +40,12 @@ const CategoryPage = () => {
                 </h2>
             </div>
 
-            <div className="grid gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 xl:gap-x-8 cursor-pointer">
-                {products.map((product) => (
-                    <a key={product.id} href={product.href} className="group border border-6 border-gray-500 rounded-lg">
-                        <img
-                            alt={product.name}
-                            src={"../src" + product.image}
-                            className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8] "
-                        />
-                        <div className="my-4 mx-4">
-                            <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                            <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-                        </div>
-                    </a>
+            <div className="flex flex-wrap">
+                {products.map(product => (
+                    <ProductCard key={product._id} product={product} />
                 ))}
                 {products.length === 0 && (
-                    <div className="col-span-full text-center py-10 text-gray-500">
+                    <div className="w-full text-center py-10 text-gray-500">
                         No products found in this category
                     </div>
                 )}
